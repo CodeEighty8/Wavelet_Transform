b017125bf636a6737b33206eb9cfae33b23b5bd3
ggeighty8 <yash.eighty8@gmail.com>
2014-06-05 11:25:00 +0530

    bug fix

--- Multi_Resolution_Analysis.py
+++ Multi_Resolution_Analysis.py
@@ -30,57 +30,63 @@ import pylab as pl
 from Haar_Transform import haar_transform as ht
 
 def MRA(s, LEVELS):
-	level = range(1, LEVELS+1)
-	scale = [2**l for l in level]
+    level = range(1, LEVELS+1)
+    scale = [2**l for l in level]
+    
+    s_coef, w_coef, n = ht(s, LEVELS)
 
-	s_coef, w_coef, n = ht(s, LEVELS)
+    ts_sc = np.zeros(s.size, dtype=float)
+    temp = 0
 
-	ts_sc = np.zeros(s.size, dtype=float)
-	temp = 0
+    while temp < scale[LEVELS - 1]:
+        ts_sc[temp::scale[LEVELS-1]] = s_coef[0][:]/np.power(np.sqrt(2), LEVELS)
+        temp += 1
+    
+    ts_wc = np.zeros((LEVELS, s.size), dtype=float)
 
-	while temp < scale[LEVELS - 1]:
-		ts_sc[temp::scale[LEVELS-1]] = s_coef[0][:]#/np.power(np.sqrt(2), LEVELS)
-		temp += 1
+    for l in level:
+        temp = 0
+        while temp < scale[LEVELS-l]:
+            if temp < scale[l-1]/2:
+                ts_wc[LEVELS - l][temp::scale[LEVELS-l]] = w_coef[l-1][:]/np.power(np.sqrt(2), l)
+                #print temp, scale[LEVELS-l], ts_wc[LEVELS - l][temp::scale[LEVELS-l]].shape, w_coef[l-1].shape
+                temp += 1
+            else:
+                ts_wc[LEVELS - l][temp::scale[LEVELS-l]] = (-w_coef[l-1][:])/np.power(np.sqrt(2), l)
+                #print temp, scale[LEVELS-l], ts_wc[LEVELS - l][temp::scale[LEVELS-l]].shape, w_coef[l-1].shape
+                temp += 1
+    
+    return np.vstack((ts_wc, ts_sc))
 
-	ts_wc = np.zeros((LEVELS, s.size), dtype=float)
 
-	for l in level:
-		temp = 0
-		while temp < scale[LEVELS-l]:
-			if temp < scale[l-1]/2:
-				ts_wc[LEVELS - l][temp::scale[LEVELS-l]] = w_coef[l-1][:]
-				print temp, scale[LEVELS-l],ts_wc[LEVELS - l][temp::scale[LEVELS-l]].shape, w_coef[l-1].shape;#/np.power(np.sqrt(2), l)
-				temp += 1
-			else:
-				ts_wc[LEVELS - l][temp::scale[LEVELS-l]] = (-w_coef[l-1][:])
-				print temp, scale[LEVELS-l],ts_wc[LEVELS - l][temp::scale[LEVELS-l]].shape, w_coef[l-1].shape;#/np.power(np.sqrt(2), l)
-				temp += 1
-
-	return np.vstack((ts_wc, ts_sc))
 
 if __name__ == "__main__":
-	s = np.load('/home/venu/programming/CAOS/Data/3b42_v7_daily_quarter_degree/trmm3b42v7_daily/Python_data_saves/daily.spatial.mean.Central.India.npy')['mean'][0:4096]
-	LEVELS = 3
-	if LEVELS == 0:
-		LEVELS = np.int(np.log2(s.size))
+    s = np.load('/home/venu/programming/CAOS/Data/3b42_v7_daily_quarter_degree/trmm3b42v7_daily/Python_data_saves/daily.spatial.mean.Central.India.npy')['mean'][0:4096]
+    
+    LEVELS = 3
 
-	k = MRA(s, LEVELS)
+    if LEVELS == 0:
+        LEVELS = np.int(np.log2(s.size))
 
-	fig, ax = pl.subplots(LEVELS+2, sharex=True)
-	for l in range(1, LEVELS+1):
-		ax[l+1].plot(k[LEVELS-l])
-		ax[l+1].set_ylabel('D'+str(LEVELS+1-l))
-		ax[l+1].grid()
-		ax[l+1].set_xlim(0, s.size-1)
+    k = MRA(s, LEVELS)
 
-		ax[1].plot(k[LEVELS], 'b')
-		ax[1].set_ylabel('A'+str(LEVELS))
-		ax[1].set_xlim(0, s.size-1)
-		ax[1].grid()
+    fig, ax = pl.subplots(LEVELS+2, sharex=True)
+    for l in range(1, LEVELS+1):
+        ax[l+1].plot(k[LEVELS-l])
+        ax[l+1].set_ylabel('D'+str(LEVELS+1-l))
+        ax[l+1].grid()
+        ax[l+1].set_xlim(0, s.size-1)
 
-		ax[0].plot(s, 'b')
-		ax[0].set_ylabel('s ($mm\ day^{-1}$)')
-		ax[0].set_xlim(0, s.size-1)
-		ax[0].grid()
+        ax[1].plot(k[LEVELS], 'b')
+        ax[1].set_ylabel('A'+str(LEVELS))
 
-pl.show()
+        ax[1].set_xlim(0, s.size-1)
+        ax[1].grid()
+
+        ax[0].plot(s, 'b')
+        ax[0].set_ylabel('s ($mm\ day^{-1}$)')
+        ax[0].set_xlim(0, s.size-1)
+        ax[0].grid()
+
+    pl.tight_layout()
+    pl.show()
